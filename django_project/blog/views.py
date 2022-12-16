9f41477e197420d8721350ff800e4b655ebc5b4d
AMALNR98 <amalnr123@gmail.com>
2022-12-16 12:09:16 +0530

    created home view and returned h1 tag with message

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Blog Home<h1>')
