3d832908c56d3ae68348132731fabbca17aec052
AMALNR98 <amalnr123@gmail.com>
2022-12-21 11:27:09 +0530

    returned render page about.html

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -7,4 +7,5 @@ def home(request):
     # return HttpResponse('<h1>Blog Home<h1>')
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html')
+    # return HttpResponse("<h1>Blog About</h1>")
