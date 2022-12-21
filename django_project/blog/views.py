b352dbcef48bf4195df2662368a43da14561318c
AMALNR98 <amalnr123@gmail.com>
2022-12-21 11:42:09 +0530

    created dummy data 'posts'

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'CoreyMs',
+        'title' : 'Blog post 1',
+        'content':'First post content',
+        'date_posted':'December 21, 2022'
+    },
+    {
+        'author' : 'jane',
+        'title' : 'Blog post 2',
+        'content':'Second post content',
+        'date_posted':'December 21, 2022'
+    },
+    {
+        'author' : 'CoreyM',
+        'title' : 'Blog post 3',
+        'content':'Third post content',
+        'date_posted':'December 21, 2022'
+    },
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
     # return HttpResponse('<h1>Blog Home<h1>')
 
 def about(request):
