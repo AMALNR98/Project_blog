c3d2be42a74d1de94dd81bfa6d3d2df4361ee9fd
AMALNR98 <amalnr123@gmail.com>
2022-12-26 10:16:32 +0530

    added users app to project

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default= timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE) #will delete user and posts as well
+
+    def __str__(self):
+        return self.title
