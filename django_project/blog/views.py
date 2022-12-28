ba6e1258bb6dcadfaf3cf55f1f9fd8770878793f
AMALNR98 <amalnr123@gmail.com>
2022-12-28 10:48:15 +0530

    created post detail view template

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -17,6 +17,13 @@ class PostListView(ListView):
     model = Post 
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.view
     context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post 
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
